385f59741e3d5628c29774270afd0fc9478efa61
HJacksons <jacksonherberts@gmail.com>
2023-10-15 01:21:32 +0200

    0

--- run_docker.py
+++ run_docker.py
@@ -7,7 +7,7 @@ def run_docker_container():
         "--name", "canario-shop",
         "-p", "8000:80",
         "-e", "SHOW_FLASHSALE=1",
-        "-e", "SHOW_PREMIUM=0",
+        "-e", "SHOW_PREMIUM=1",
         "-e", "USE_MEMCACHE=Y",
         "-e", "MEMCACHE_SERVER=10.196.38.192", #host.docker.internal
         "-d", "canario-shop:latest"
