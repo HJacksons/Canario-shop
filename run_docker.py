943b78bbcd6a5cccb1a3125d0cf92dac5d0480d2
HJacksons <jacksonherberts@gmail.com>
2023-10-15 01:01:49 +0200

    update flag

--- run_docker.py
+++ run_docker.py
@@ -6,8 +6,8 @@ def run_docker_container():
         "docker", "run",
         "--name", "canario-shop",
         "-p", "8000:80",
-        "-e", "SHOW_FLASHSALE=0",
-        "-e", "SHOW_PREMIUM=1",
+        "-e", "SHOW_FLASHSALE=1",
+        "-e", "SHOW_PREMIUM=0",
         "-e", "USE_MEMCACHE=Y",
         "-e", "MEMCACHE_SERVER=10.196.38.192", #host.docker.internal
         "-d", "canario-shop:latest"
