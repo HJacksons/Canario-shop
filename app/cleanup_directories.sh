9dd68c3a98ac32ac3de549ac1817900b37cfadff
Jackson Herbert Sinamenye <s371140@oslomet.no>
2023-10-31 01:16:49 +0100

    Make cleanup script executable

--- app/cleanup_directories.sh
+++ app/cleanup_directories.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-# Remove unwanted directories
-rm -rf .pytest_cache __pycache__
+echo "Starting cleanup..."
+
+if [ -d "/app/.pytest_cache" ]; then
+    echo "Found .pytest_cache directory. Attempting to remove..."
+    rm -rf /app/.pytest_cache
+else
+    echo ".pytest_cache directory not found."
+fi
+
+if [ -d "/app/__pycache__" ]; then
+    echo "Found __pycache__ directory. Attempting to remove..."
+    rm -rf /app/__pycache__
+else
+    echo "__pycache__ directory not found."
+fi
+
+echo "Cleanup complete."
